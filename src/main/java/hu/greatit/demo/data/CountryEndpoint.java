48fead2b5b3b8b5c10f5da63162cae09b47d2936
Csanad Kovacs <csanad@Csanads-MacBook-Pro.local>
2018-01-13 23:59:54 +0100

    first commit

---
+++ src/main/java/hu/greatit/demo/data/CountryEndpoint.java
@@ -0,0 +1,31 @@
+package hu.greatit.demo.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import hu.greatit.demo.guides.ws_country.GetCountryRequest;
+import hu.greatit.demo.guides.ws_country.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://demo.greatit.hu/guides/ws-country";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
